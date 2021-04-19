5507c05751763d2c2cf866e0ade1cf734ae831ac
dennisgbrown <dennis.g.brown@gmail.com>
2021-04-19 17:18:48 -0500

    graph stuff. express compiled data as precentages.

--- util/resultinator.py
+++ util/resultinator.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 """
+### Compile and plot results from VAD runs.
+###
 ### Authors: Dennis Brown, Shannon McDade, Jacob Parmer
 ###
 ### Created: April 15, 2021
 """
 
 import csv
+import plotly.graph_objects as go
+import numpy as np
 
 def compile_results(input_folder, target_noise_loudnesses, output_fileroot):
     outfile1 = open(output_fileroot + '_acc.csv', "w")
@@ -17,6 +21,13 @@ def compile_results(input_folder, target_noise_loudnesses, output_fileroot):
     datalist = ['test-clean']
     for loudness in target_noise_loudnesses:
         datalist.append('test-clean-db' + str(loudness))
+
+    num_data_sets = len(datalist)
+    acc = np.zeros((num_data_sets * num_data_sets, 3))
+    psas = np.zeros((num_data_sets * num_data_sets, 3))
+    psan = np.zeros((num_data_sets * num_data_sets, 3))
+    pnas = np.zeros((num_data_sets * num_data_sets, 3))
+    pnan = np.zeros((num_data_sets * num_data_sets, 3))
 
     outfile1.write('dataset\\model')
     outfile2.write('dataset\\model')
@@ -35,12 +46,14 @@ def compile_results(input_folder, target_noise_loudnesses, output_fileroot):
     outfile4.write('\n')
     outfile5.write('\n')
 
+    dataset_id = 0
     for dataset in datalist:
         outfile1.write(dataset)
         outfile2.write(dataset)
         outfile3.write(dataset)
         outfile4.write(dataset)
         outfile5.write(dataset)
+        model_id = 0
         for model in datalist:
             input_filename = input_folder + 'results_model_' + model + '_data_' + dataset + '_cm.csv'
             # print('Reading', input_filename)
@@ -67,26 +80,80 @@ def compile_results(input_folder, target_noise_loudnesses, output_fileroot):
             outfile3.write(', %.2f' % (total_psan / event_count))
             outfile4.write(', %.2f' % (total_pnas / event_count))
             outfile5.write(', %.2f' % (total_pnan / event_count))
+            acc[dataset_id * num_data_sets + model_id][0] = dataset_id
+            acc[dataset_id * num_data_sets + model_id][1] = model_id
+            acc[dataset_id * num_data_sets + model_id][2] = total_acc / signal_count
+            psas[dataset_id * num_data_sets + model_id][0] = dataset_id
+            psas[dataset_id * num_data_sets + model_id][1] = model_id
+            psas[dataset_id * num_data_sets + model_id][2] = total_psas / event_count * 100
+            psan[dataset_id * num_data_sets + model_id][0] = dataset_id
+            psan[dataset_id * num_data_sets + model_id][1] = model_id
+            psan[dataset_id * num_data_sets + model_id][2] = total_psan / event_count * 100
+            pnas[dataset_id * num_data_sets + model_id][0] = dataset_id
+            pnas[dataset_id * num_data_sets + model_id][1] = model_id
+            pnas[dataset_id * num_data_sets + model_id][2] = total_pnas / event_count * 100
+            pnan[dataset_id * num_data_sets + model_id][0] = dataset_id
+            pnan[dataset_id * num_data_sets + model_id][1] = model_id
+            pnan[dataset_id * num_data_sets + model_id][2] = total_pnan / event_count * 100
             infile.close()
+            model_id += 1
         outfile1.write('\n')
         outfile2.write('\n')
         outfile3.write('\n')
         outfile4.write('\n')
         outfile5.write('\n')
-
+        dataset_id += 1
     outfile1.close()
     outfile2.close()
     outfile3.close()
     outfile4.close()
     outfile5.close()
 
+    return acc, psas, psan, pnas, pnan
 
 def main():
+
+    # Get/write data
     input_folder = '../results/'
-    target_noise_loudnesses = [-160, -80, -40, -20, -10, -5, 0, 5]
+    target_noise_loudnesses = [-160, -80, -40, -20, -10, -5, 0]
     output_fileroot = '../results/compiled_results'
-    compile_results(input_folder, target_noise_loudnesses, output_fileroot)
+    acc, psas, psan, pnas, pnan = compile_results(input_folder, target_noise_loudnesses, output_fileroot)
     print('Done -- see files starting with', output_fileroot)
+
+    # Plots
+    labels = ['orig']
+    for noise in target_noise_loudnesses: labels.append(str(noise))
+
+    accx, accy, accz = acc.T
+    psasx, psasy, psasz = psas.T
+    psanx, psany, psanz = psan.T
+    pnasx, pnasy, pnasz = pnas.T
+    pnanx, pnany, pnanz = pnan.T
+
+    fig = go.Figure(data=[go.Mesh3d(name='Accuracy', x=accx, y=accy, z=accz, color='red', opacity=0.50),
+                          go.Mesh3d(name='PSAS', x=psasx, y=psasy, z=psasz, color='blue', opacity=0.50),
+                          go.Mesh3d(name='PSAN', x=psanx, y=psany, z=psanz, color='green', opacity=0.50),
+                          go.Mesh3d(name='PNAS', x=pnasx, y=pnasy, z=pnasz, color='yellow', opacity=0.50),
+                          go.Mesh3d(name='PNAN', x=pnanx, y=pnany, z=pnanz, color='purple', opacity=0.50)])
+    fig.update_traces(showlegend = True)
+    fig.update_layout(
+        scene = dict(
+            xaxis = dict(
+                tickmode = 'array',
+                tickvals = list(range(len(labels))),
+                ticktext = labels
+            ),
+            yaxis = dict(
+                tickmode = 'array',
+                tickvals = list(range(len(labels))),
+                ticktext = labels
+            ),
+            xaxis_title='Data set',
+            yaxis_title='Model',
+            zaxis_title='Percent'
+        )
+    )
+    fig.show()
 
 
 if __name__ == '__main__':
